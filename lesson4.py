7eff81c720d4f3d28e52f8a368f312a9dbae3445
Amanovmirlan <amanov230893@gmail.com>
2022-07-15 11:00:34 +0600

    first commit

---
+++ lesson4.py
@@ -0,0 +1,23 @@
+# file = open("first.txt", "r")
+# print(file.read())
+
+# file = open("second.txt", "w")
+# file.write("hi how are you?")
+# file.close()
+#
+# file = open("second.txt", "+r")
+# # file.writelines("i am fine!")
+# print (file.read())
+# file.write("acssa, asdsad")
+# file.close()
+
+# file = open("second.txt", "w")
+# for i in range(1, 1000000):
+#     file.write(f"{i} ")
+# file.close()
+
+# from math import sqrt as square
+#
+# # with open('first.txt', 'w') as file:
+# #     file.writelines(['Not bad)', '\n\tand you!'])
+# print (square(4))
